02441622777e95cf9e6cf5e107516f0dfcdbe840
Christopher Lamont <c.lamont.dev@gmail.com>
2018-01-21 18:06:40 +0100

    Forms app added

---
+++ XamLocalization/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace XamLocalization
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new XamLocalizationPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamLocalization/XamLocalizationPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamLocalization
+{
+    public partial class XamLocalizationPage : ContentPage
+    {
+        public XamLocalizationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
